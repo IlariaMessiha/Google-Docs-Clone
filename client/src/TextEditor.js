f3435e17492f3aef28f5aaa2aaae0021db39c548
Ilaria Refaat <ilariarefaa20@gmail.com>
2022-05-12 00:31:33 +0200

    Handling multiple documents

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useState } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
 import { io } from "socket.io-client";
+import { useParams } from "react-router-dom";
 const TOOLBAR_OPTIONS = [
   [{ header: [1, 2, 3, 4, false] }],
   ["bold", "italic", "underline"],
@@ -11,8 +12,10 @@ const TOOLBAR_OPTIONS = [
 ];
 
 export default function TextEditor() {
+  const { id: documentID } = useParams();
   const [socket, setSocket] = useState();
   const [quill, setQuill] = useState();
+
   useEffect(() => {
     const s = io("http://localhost:3001");
     setSocket(s);
@@ -20,6 +23,14 @@ export default function TextEditor() {
       s.disconnect();
     };
   }, []);
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+    socket.once("load-document", (document) => {
+      quill.setContents(document);
+      quill.enable();
+    });
+    socket.emit("get-document", documentID);
+  }, [socket, quill, documentID]);
   //updating our doc with the changes that are changed from other client
   useEffect(() => {
     if (socket == null || quill == null) return;
@@ -55,6 +66,8 @@ export default function TextEditor() {
       theme: "snow",
       modules: { toolbar: TOOLBAR_OPTIONS },
     });
+    q.disable();
+    q.setText("Loading...");
     setQuill(q);
   }, []);
   return <div className="container" ref={wrapperRef}></div>;
--- server/server.js
+++ server/server.js
@@ -6,8 +6,13 @@ const io = require("socket.io")(3001, {
 });
 //every time our client connects this function is executed
 io.on("connection", (socket) => {
-  //listening to changes from the client
-  socket.on("send-changes", (delta) => {
-    socket.broadcast.emit("receive-changes", delta);
+  socket.on("get-document", (documentID) => {
+    const data = "";
+    socket.join(documentID);
+    socket.emit("load-document", data);
+    //listening to changes from the client
+    socket.on("send-changes", (delta) => {
+      socket.broadcast.to(documentID).emit("receive-changes", delta);
+    });
   });
 });
